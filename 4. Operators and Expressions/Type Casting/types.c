7549b1359eb9b3e47157ec3d3d0b207bb4584e3d
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-29 21:24:13 +0545

    type casting

--- 4. Operators and Expressions/Type Casting/types.c
+++ 4. Operators and Expressions/Type Casting/types.c
@@ -3,9 +3,12 @@ int main()
 {
     int a = 10, b = 3;
     // implicit type casting
+    // implicit ma user le nei handle garna parxa type conversion
     printf("a/b = %f \n", (float)a / b); // float / integer = float, integer/integer = integer
 
     int c = 12.3; // explicit type casting
+    // explicit typecasting ma compiler le handle garxa type conversions
+
     printf("c = %d\n", c);
 
     int d = 65;            // ASCII Table
